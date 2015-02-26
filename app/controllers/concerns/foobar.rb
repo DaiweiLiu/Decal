89f372759157bcef7250adea175ca25b9cbee68d
DaiweiLiu <daiwei.liu@berkeley.edu>
2015-02-26 10:43:16 -0800

    Completed HW2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,2 +1,5 @@
 class Foobar
+	def self.baz(ary)
+		return ary.map(&:to_i).map{|x| x+2}.select{|x| x%2==0}.uniq.select{|x| x<=10}.inject(:+)
+	end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,27 @@
 class PagesController < ApplicationController
   def home
-    # @baz = Foobar.baz ["1", "2", "3", "3", "4", "5", "10", "11", "100"]
+    @baz = Foobar.baz ["1", "2", "3", "3", "4", "5", "10", "11", "100"]
   end
 
   def stringify
-    @text = "You are nothing!"
+    if not params.blank?
+      @text = params[:name] + " is " + params[:adjective]
+    else
+      @text = "You are nothing!"
+    end
   end
 
   def age
   end
 
   def person
+    if not params.blank?
+      @p = Person.new(params[:name], params[:age])
+      @intro = @p.introduce()
+      @birth_year = @p.birth_year()
+      @nickname = @p.nickname()
+    else
+      @p = nil
+    end
   end
 end
